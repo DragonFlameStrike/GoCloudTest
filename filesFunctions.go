2656f6b98e7a303ed3343c9438b9d6d8b11f7eb5
d.pankov <d.pankov@g.nsu.ru>
2022-11-04 14:30:39 +0700

    refactoring

--- filesFunctions.go
+++ filesFunctions.go
@@ -10,6 +10,51 @@ import (
 	"strings"
 )
 
+func ReceiveFile(w http.ResponseWriter, r *http.Request) {
+	r.ParseMultipartForm(32 << 20) // limit your max input length!
+	file, header, err := r.FormFile("file")
+	if err != nil {
+		iLog.Fatal(err)
+		return
+	}
+	defer file.Close()
+	iLog.Printf("Get file - %s\n", header.Filename)
+	// Copy the file data to my buffer
+	name := strings.Split(header.Filename, ".")
+	newFile, err := os.Create("./configs/" + name[0] + "_v1.0." + name[1])
+	if err != nil {
+		iLog.Println("Can't create new file")
+		return
+	}
+	defer newFile.Close()
+	_, err = io.Copy(newFile, file)
+	if err != nil {
+		iLog.Println("Can't copy in new file")
+		return
+	}
+	return
+}
+
+func RequestFile(w http.ResponseWriter, r *http.Request) {
+	s := r.URL.Query().Get("service")
+	files := findFiles(s)
+	if len(files) == 0 {
+		iLog.Printf("NOT SUCCESS : config is not found")
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+	if s == "" { //Print all configs
+		for i, file := range files {
+			fmt.Fprintf(w, "%d. %s\n", i+1, file)
+		}
+		iLog.Printf("SUCCESS : configs is sent")
+	} else { //Print config by service name
+		file := chooseNewestFile(files)
+		iLog.Printf("SUCCESS : config is found - ", file)
+		http.ServeFile(w, r, "./configs/"+file)
+	}
+}
+
 func chooseNewestFile(files []string) string {
 	maxV := "0.0"
 	maxVId := -1
@@ -73,49 +118,4 @@ func extractValue(body string, key string) string {
 	match := r.FindString(body)
 	keyValMatch := strings.Split(match, ":")
 	return strings.ReplaceAll(keyValMatch[1], "\"", "")
-}
-
-func ReceiveFile(w http.ResponseWriter, r *http.Request) {
-	r.ParseMultipartForm(32 << 20) // limit your max input length!
-	file, header, err := r.FormFile("file")
-	if err != nil {
-		iLog.Fatal(err)
-		return
-	}
-	defer file.Close()
-	iLog.Printf("Get file - %s\n", header.Filename)
-	// Copy the file data to my buffer
-	name := strings.Split(header.Filename, ".")
-	newFile, err := os.Create("./configs/" + name[0] + "_v1.0." + name[1])
-	if err != nil {
-		iLog.Println("Can't create new file")
-		return
-	}
-	defer newFile.Close()
-	_, err = io.Copy(newFile, file)
-	if err != nil {
-		iLog.Println("Can't copy in new file")
-		return
-	}
-	return
-}
-
-func RequestFile(w http.ResponseWriter, r *http.Request) {
-	s := r.URL.Query().Get("service")
-	files := findFiles(s)
-	if len(files) == 0 {
-		iLog.Printf("NOT SUCCESS : config is not found")
-		http.Error(w, "404 not found.", http.StatusNotFound)
-		return
-	}
-	if s == "" { //Print all configs
-		for i, file := range files {
-			fmt.Fprintf(w, "%d. %s\n", i+1, file)
-		}
-		iLog.Printf("SUCCESS : configs is sent")
-	} else { //Print config by service name
-		file := chooseNewestFile(files)
-		iLog.Printf("SUCCESS : config is found - ", file)
-		http.ServeFile(w, r, "./configs/"+file)
-	}
 }
